8dcb63bc6327e3cc6fd930cb1a1f29e35eaa23ba
KhiroyaN <93214971+NeelthebigGuy@users.noreply.github.com>
2024-03-12 01:23:04 -0300

    Add files via upload

--- cleanup.sql
+++ cleanup.sql
@@ -1,5 +1,5 @@
 -- Checking for already made datebase
-DROP DATABASE IF EXISTS lol_champs_db;
+DROP DATABASE IF EXISTS lol_database;
 
 -- Checking for already made user
 DROP USER IF EXISTS 'lol_user'@'localhost';
